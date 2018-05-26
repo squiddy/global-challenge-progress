c78bddc144af115dcb7a9675c7faee78d0e7db67
Reiner Gerecke <me@reinergerecke.de>
2018-05-26 05:16:40 +0200

    Format code using black
    Ever since I've gotten used to prettier formatting my js code, it is
    awkward having to format python code myself. Let's give black a try.

--- create_image.py
+++ create_image.py
@@ -6,36 +6,40 @@ import requests
 
 def login(username, password):
     session = requests.Session()
-    session.post('https://globalchallenge.virginpulse.com/log-in', data={
-        'username': username,
-        'password': password
-    })
+    session.post(
+        "https://globalchallenge.virginpulse.com/log-in",
+        data={"username": username, "password": password},
+    )
     return session
 
 
 def get_own_team(session):
-    res = session.post('https://globalchallenge.virginpulse.com/event/map/visited/null')
-    return res.json()['MyMarker']
+    res = session.post("https://globalchallenge.virginpulse.com/event/map/visited/null")
+    return res.json()["MyMarker"]
 
 
 def get_other_teams(session):
-    res = session.post('https://globalchallenge.virginpulse.com/event/map/organisation-teams')
+    res = session.post(
+        "https://globalchallenge.virginpulse.com/event/map/organisation-teams"
+    )
     return res.json()
 
 
-session = login(os.environ['VIRGIN_USERNAME'], os.environ['VIRGIN_PASSWORD'])
+session = login(os.environ["VIRGIN_USERNAME"], os.environ["VIRGIN_PASSWORD"])
 my_team = get_own_team(session)
 other_teams = get_other_teams(session)
 
-my_team = {'name': my_team['Name'], 'steps': int(my_team['StepTotalFormatted'].replace('.', ''))}
+my_team = {
+    "name": my_team["Name"],
+    "steps": int(my_team["StepTotalFormatted"].replace(".", "")),
+}
 teams = [my_team]
 for team in other_teams:
-    teams.append({
-        'name': team['Name'], 'steps': team['Steps']
-    })
+    teams.append({"name": team["Name"], "steps": team["Steps"]})
 
 
-print('''<?xml version="1.0" encoding="UTF-8" ?>
+print(
+    """<?xml version="1.0" encoding="UTF-8" ?>
 <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
   <style>
     * { fill: #fff }
@@ -45,14 +49,17 @@ print('''<?xml version="1.0" encoding="UTF-8" ?>
     .my-team .steps, .my-team .name { fill: #87d0f0; }
   </style>
   <text x="100" y="20" class="heading">Challenge</text>
-''')
+"""
+)
 
-for i, t in enumerate(sorted(teams, key=lambda e: e['steps'], reverse=True)):
-    print(f'''
+for i, t in enumerate(sorted(teams, key=lambda e: e["steps"], reverse=True)):
+    print(
+        f"""
     <g class="{'my-team' if t['name'] == my_team['name'] else ''}">
         <text x="10" y="{i * 20 + 60}" class="steps">{t['steps']}</text>
         <text x="100" y="{i * 20 + 60}" class="name">{t['name']}</text>
     </g>
-    ''')
+    """
+    )
 
-print('</svg>')
+print("</svg>")
